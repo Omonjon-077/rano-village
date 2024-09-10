174eebae45b41fdaf1f842c41fdc5621430b3423
Omonjon <omonjons077@gmail.com>
2024-09-10 11:44:32 +0500

    New section added and done to mobile version

--- assets/script/main.js
+++ assets/script/main.js
@@ -109,7 +109,7 @@ if ($(".partnersSwiper").length) {
 if ($("#scroll-up").length) {
     const scrollUp = () => {
         const scrollUp = document.getElementById('scroll-up')
-        // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+        // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll up class
         this.scrollY >= 300 ? scrollUp.classList.add('show-scroll')
             : scrollUp.classList.remove('show-scroll')
     }
